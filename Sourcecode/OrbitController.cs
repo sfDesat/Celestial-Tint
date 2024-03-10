0182250998bdf44aad7b596d38581b91c27add8f
sfDesat <75356901+sfDesat@users.noreply.github.com>
2024-03-10 08:54:23 +0100

    Update OrbitController.cs

--- Sourcecode/OrbitController.cs
+++ Sourcecode/OrbitController.cs
@@ -14,18 +14,21 @@ public class OrbitController : MonoBehaviour
     public Vector3 planetPosition;
     public float orbitHeight = 80000f;
     public float planetRadius = 6378100f;
-    public Cubemap planetTexture;
     [Space]
     public float sunStartMin;
     public float sunStartMax;
+    [Space]
+    public GameObject meteorShower;
+    public float meteorShowerChance = 3f;
 
     [Header("Colorization Settings")]
+    public Texture2D planetTexture; 
     public Color planetTint;
     public Color airTint;
 
     [Header("Material Settings")]
-    public Material planetMaterial;
-    public Cubemap assignedCubemap;
+    public Material planetMaterial; // Use the material with the ShaderGraph
+    public CustomRenderTexture planetRenderTexture;
 
     [Header("References")]
     public GameObject sun;
@@ -34,6 +37,28 @@ public class OrbitController : MonoBehaviour
     public Volume skyVolume;
 
     private PhysicallyBasedSky sky;
+
+    private void OnValidate()
+    {
+        Debug.Log("[OrbitController] OnVoid called");
+
+        // Set color in the shader
+        if (planetMaterial != null)
+        {
+            planetMaterial.SetColor("_SurfaceColor", planetTint);
+            planetMaterial.SetTexture("_SurfaceTexture", planetTexture);
+
+            Debug.Log("[OrbitController] Shader properties set successfully");
+        }
+        else
+        {
+            Debug.LogError("[OrbitController] planetMaterial is null. Cannot set shader properties.");
+        }
+
+        planetRenderTexture.Update();
+        planetRenderTexture.Initialize();
+    }
+
 
     private void Awake()
     {
@@ -51,16 +76,23 @@ public class OrbitController : MonoBehaviour
                 sky.planetRotation.value = planetRotateStartVector;
 
                 // Apply tints
-                sky.groundTint.value = planetTint;
                 sky.airTint.value = airTint;
+                sky.groundTint.value = planetTint;
 
-                // Use the assigned Cubemap directly
-                sky.groundColorTexture.value = assignedCubemap;
-                sky.groundEmissionTexture.value = assignedCubemap;
+                planetRenderTexture.Update();
+                planetRenderTexture.Initialize();
+
+                // Set color in the shader
+                planetMaterial.SetColor("_SurfaceColor", planetTint);
+                planetMaterial.SetTexture("_SurfaceTexture", planetTexture);
 
                 // Apply Orbit
                 sky.planetCenterPosition.value = new Vector3(0, -(planetRadius + orbitHeight), 0);
                 planetPosition = sky.planetCenterPosition.value;
+
+                // Set MeteorShower
+                if (Random.Range(1, meteorShowerChance) == 2)
+                    meteorShower.active = true;
             }
             else
             {
@@ -73,5 +105,20 @@ public class OrbitController : MonoBehaviour
         }
 
         Debug.Log("[OrbitController] Awake completed");
+    }
+
+    public void FixedUpdate()
+    {
+        sun.transform.Rotate(sunRotateVector);
+        moon.transform.Rotate(moonRotateVector);
+
+        sky.planetRotation.value += planetRotateVector;
+
+        if (planetRenderTexture != null)
+        {
+            //planetRenderTexture.Update();
+            //planetRenderTexture.Initialize();
+            //Debug.Log("CRT applied");
+        }
     }
 }
