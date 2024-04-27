2fdf2681f266d35e9aea45672ed1b9d612290d17
sfDesat <75356901+sfDesat@users.noreply.github.com>
2024-04-27 19:13:48 +0200

    v1.3.0

--- Sourcecode/CelestialTint.cs
+++ Sourcecode/CelestialTint.cs
@@ -1,12 +1,19 @@
-﻿using BepInEx;
+using BepInEx;
+using BepInEx.Configuration;
+using System.Collections.Generic;
 using UnityEngine;
 
-[BepInPlugin("CelestialTint", "Celestial Tint", "1.1.13")]
+[BepInPlugin("CelestialTint", "Celestial Tint", "1.3.0")]
 public class CelestialTint : BaseUnityPlugin
 {
+    internal static CTConfig ModConfig; // Change to internal accessibility
+
     private void Awake()
     {
         Debug.Log("[Celestial Tint] Loading complete");
+
+        // Load config
+        ModConfig = new CTConfig(Config); // Pass ConfigFile instance to CTConfig
 
         // Initialize the loader
         CelestialTintLoader.Initialize();
