8d09a4dde126c87fac3cb3b8e4e99cfc5803eb25
sfDesat <75356901+sfDesat@users.noreply.github.com>
2024-04-29 11:47:09 +0200

    v1.3.2

--- Sourcecode/CTConfig.cs
+++ Sourcecode/CTConfig.cs
@@ -14,7 +14,7 @@ public class CTConfig
         // Adjust the description here
         DebugLogging = configFile.Bind("3. Debugging", "DebugLogging", false, "Whether to display debug messages. When false, Celestial Tint will not display any messages.");
         PlanetTagMappings = configFile.Bind("1. Planets", "PlanetTagMappings", "", "Mappings of planet names and tags. Separate each mapping with a comma. Each mapping should be in the format 'PlanetName@TagName'. Example: Experimentation@Wasteland, Vow@Valley. Go to the Celestial Tint github wiki page for previews.");
-        DisplayShipParts = configFile.Bind("2. Ship", "DisplayShipParts", false, "[Non-Functional] Whether to display ship parts in orbit. Only visible if you go outside.");
+        DisplayShipParts = configFile.Bind("2. Ship", "DisplayShipParts", false, " Whether to display ship parts in orbit. Only visible if you go outside.");
         ShipDoorAccess = configFile.Bind("2. Ship", "ShipDoorAccess", false, "Whether to door can be opened while in orbit.");
     }
 }
