1195f7eea32e688b4561d41231897a810682d401
sfDesat <75356901+sfDesat@users.noreply.github.com>
2024-04-29 17:03:27 +0200

    v1.4.1

--- Sourcecode/CTConfig.cs
+++ Sourcecode/CTConfig.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 
 public class CTConfig
 {
+    public ConfigEntry<bool> VanillaMode { get; set; }
     public ConfigEntry<string> PlanetTagMappings { get; set; }
     public ConfigEntry<bool> DisplayShipParts { get; set; }
     public ConfigEntry<bool> ShipDoorAccess { get; set; }
@@ -11,10 +12,10 @@ public class CTConfig
 
     public CTConfig(ConfigFile configFile)
 	{
-        // Adjust the description here
-        DebugLogging = configFile.Bind("3. Debugging", "DebugLogging", false, "Whether to display debug messages. When false, Celestial Tint will not display any messages.");
+        VanillaMode = configFile.Bind("1. Planets", "VanillaMode", false, "Whether the outside should show the vanilla planets. Also adds in a sun object. Illusion can break when enabling ShipDoorAccess.");
         PlanetTagMappings = configFile.Bind("1. Planets", "PlanetTagMappings", "", "Mappings of planet names and tags. Separate each mapping with a comma. Each mapping should be in the format 'PlanetName@TagName'. Example: Experimentation@Wasteland, Vow@Valley. Go to the Celestial Tint github wiki page for previews.");
         DisplayShipParts = configFile.Bind("2. Ship", "DisplayShipParts", false, " Whether to display ship parts in orbit. Only visible if you go outside.");
         ShipDoorAccess = configFile.Bind("2. Ship", "ShipDoorAccess", false, "Whether to door can be opened while in orbit.");
+        DebugLogging = configFile.Bind("3. Debugging", "DebugLogging", false, "Whether to display debug messages. When false, Celestial Tint will not display any messages.");
     }
 }
