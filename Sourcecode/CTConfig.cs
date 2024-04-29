3230e4ee042788200301a06db52e6be5ec47f0c3
sfDesat <75356901+sfDesat@users.noreply.github.com>
2024-04-29 09:45:28 +0200

    v1.3.1

--- Sourcecode/CTConfig.cs
+++ Sourcecode/CTConfig.cs
@@ -4,11 +4,17 @@ using System.Collections.Generic;
 
 public class CTConfig
 {
-	public ConfigEntry<string> PlanetTagMappings { get; set; }
+    public ConfigEntry<string> PlanetTagMappings { get; set; }
+    public ConfigEntry<bool> DisplayShipParts { get; set; }
+    public ConfigEntry<bool> ShipDoorAccess { get; set; }
+    public ConfigEntry<bool> DebugLogging { get; set; }
 
-	public CTConfig(ConfigFile configFile)
+    public CTConfig(ConfigFile configFile)
 	{
-		// Adjust the description here
-		PlanetTagMappings = configFile.Bind("General", "PlanetTagMappings", "", "Mappings of planet names and tags. Separate each mapping with a comma. Each mapping should be in the format 'PlanetName@TagName'. Example: Experimentation@Wasteland, Vow@Valley. Go to the Celestial Tint github wiki page for previews.");
-	}
+        // Adjust the description here
+        DebugLogging = configFile.Bind("3. Debugging", "DebugLogging", false, "Whether to display debug messages. When false, Celestial Tint will not display any messages.");
+        PlanetTagMappings = configFile.Bind("1. Planets", "PlanetTagMappings", "", "Mappings of planet names and tags. Separate each mapping with a comma. Each mapping should be in the format 'PlanetName@TagName'. Example: Experimentation@Wasteland, Vow@Valley. Go to the Celestial Tint github wiki page for previews.");
+        DisplayShipParts = configFile.Bind("2. Ship", "DisplayShipParts", false, "[Non-Functional] Whether to display ship parts in orbit. Only visible if you go outside.");
+        ShipDoorAccess = configFile.Bind("2. Ship", "ShipDoorAccess", false, "Whether to door can be opened while in orbit.");
+    }
 }
