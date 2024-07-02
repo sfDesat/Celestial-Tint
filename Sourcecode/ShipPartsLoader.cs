9c0c5d505f8cedc103b70631273fa4a52fbeefc1
sfDesat <75356901+sfDesat@users.noreply.github.com>
2024-07-02 13:49:03 +0200

    v1.5.2

--- Sourcecode/ShipPartsLoader.cs
+++ Sourcecode/ShipPartsLoader.cs
@@ -7,56 +7,32 @@ public static class ShipPartsLoader
 {
     private static List<GameObject> spacePropsCopies = new List<GameObject>();
 
-    // Define the propNames list here
     private static List<string> propNames = new List<string> { "ShipLightsPost", "OutsideShipRoom", "ThrusterBackLeft", "ThrusterBackRight", "ThrusterFrontLeft", "ThrusterFrontRight", "SideMachineryLeft", "SideMachineryRight", "ShipSupportBeams", "ShipSupportBeams.001", "MeterBoxDevice.001" };
 
     public static void Initialize()
     {
-        if (CelestialTint.ModConfig.DebugLogging.Value) Debug.Log("[CT Ship Parts Loader] Parts Loading");
+        if (CelestialTintStart.ModConfig.DebugLogging.Value) Debug.Log("[CT ShipPartsLoader] Parts Loading");
 
         SceneManager.sceneLoaded += OnSceneLoaded;
         SceneManager.sceneUnloaded += OnSceneUnloaded;
-
-        // Check the initial state of the scene
-        CheckSceneState();
-    }
-
-    private static void CheckSceneState()
-    {
-        // Check if SampleSceneRelay is the only scene loaded
-        if (SceneManager.sceneCount == 1 && SceneManager.GetActiveScene().name == "SampleSceneRelay")
-        {
-            ActivateSpaceProps();
-            if (CelestialTint.ModConfig.DebugLogging.Value) Debug.Log("[CT Ship Parts Loader] Activating Parts");
-        }
-        else if (SceneManager.GetActiveScene().name == "SampleSceneRelay")
-        {
-            DeactivateSpaceProps();
-            if (CelestialTint.ModConfig.DebugLogging.Value) Debug.Log("[CT Ship Parts Loader] Deactivating Parts");
-        }
-        else
-        {
-            spacePropsCopies.Clear();
-        }
     }
 
     private static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        // Check the scene state after it's loaded
-        CheckSceneState();
+        bool isInOrbit = CelestialTintStart.CheckSceneState();
+
+        if (isInOrbit) ActivateSpaceProps();
+        else if (SceneManager.GetActiveScene().name == "SampleSceneRelay") DeactivateSpaceProps();
+        else spacePropsCopies.Clear();
     }
 
-    private static IEnumerator DelayedCheckSceneState()
+    private static async void OnSceneUnloaded(Scene scene)
     {
-        // Wait for the current frame to end before checking the scene state
-        yield return new WaitForEndOfFrame();
-        CheckSceneState();
-    }
+        bool isInOrbit = await CelestialTintStart.DelayedCheckSceneStateAsync();
 
-    private static void OnSceneUnloaded(Scene scene)
-    {
-        // Delay the execution of CheckSceneState to ensure the scene has been fully unloaded
-        CoroutineHandler.Instance.StartCoroutine(DelayedCheckSceneState());
+        if (isInOrbit) ActivateSpaceProps();
+        else if (SceneManager.GetActiveScene().name == "SampleSceneRelay") DeactivateSpaceProps();
+        else spacePropsCopies.Clear();
     }
 
     private static void ActivateSpaceProps()
@@ -66,7 +42,7 @@ public static class ShipPartsLoader
             GameObject copy = spacePropsCopies.Find(c => c.name == propName + "_copy");
             if (copy != null)
             {
-                copy.SetActive(true); // Reactivate the existing copy
+                copy.SetActive(true);
             }
             else
             {
@@ -74,13 +50,13 @@ public static class ShipPartsLoader
                 if (originalProp != null)
                 {
                     copy = Object.Instantiate(originalProp, originalProp.transform.position, originalProp.transform.rotation);
-                    copy.name = propName + "_copy"; // Set a distinct name for the copy
+                    copy.name = propName + "_copy";
                     copy.SetActive(true);
-                    spacePropsCopies.Add(copy); // Add the copy to the list
+                    spacePropsCopies.Add(copy);
                 }
                 else
                 {
-                    Debug.LogError("GameObject with name '" + propName + "' not found.");
+                    Debug.LogError("[CT ShipPartsLoader] GameObject with name '" + propName + "' not found.");
                 }
             }
         }
@@ -90,26 +66,7 @@ public static class ShipPartsLoader
     {
         foreach (GameObject copy in spacePropsCopies)
         {
-            copy.SetActive(false); // Deactivate the copy
-        }
-    }
-
-
-    // Coroutine handler to allow starting coroutines in static methods
-    public class CoroutineHandler : MonoBehaviour
-    {
-        private static CoroutineHandler _instance;
-        public static CoroutineHandler Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    _instance = new GameObject("CoroutineHandler").AddComponent<CoroutineHandler>();
-                    DontDestroyOnLoad(_instance.gameObject);
-                }
-                return _instance;
-            }
+            copy.SetActive(false);
         }
     }
 }
