105d3be614620623978b74f28d71fbfd7de6d2b8
sfDesat <75356901+sfDesat@users.noreply.github.com>
2024-07-02 13:50:21 +0200

    v1.5.2

--- Sourcecode/VanillaMode.cs
+++ Sourcecode/VanillaMode.cs
@@ -10,55 +10,34 @@ public static class VanillaMode
 
     public static void Initialize()
     {
-        if (CelestialTint.ModConfig.DebugLogging.Value) Debug.Log("[CT VanillaMode] Parts Loading");
+        if (CelestialTintStart.ModConfig.DebugLogging.Value) Debug.Log("[CT VanillaMode] Parts Loading");
 
         SceneManager.sceneLoaded += OnSceneLoaded;
         SceneManager.sceneUnloaded += OnSceneUnloaded;
-
-        // Check the initial state of the scene
-        VanillaMode.CheckSceneState();
-    }
-
-    private static void CheckSceneState()
-    {
-        // Check if SampleSceneRelay is the only scene loaded
-        if (SceneManager.sceneCount == 1 && SceneManager.GetActiveScene().name == "SampleSceneRelay")
-        {
-            ActivateSun();
-            if (CelestialTint.ModConfig.DebugLogging.Value) Debug.Log("[CT VanillaMode] Activating Sun");
-        }
-        else if (SceneManager.GetActiveScene().name == "SampleSceneRelay")
-        {
-            DeactivateSun();
-            if (CelestialTint.ModConfig.DebugLogging.Value) Debug.Log("[CT VanillaMode] Deactivating Sun");
-        }
     }
 
     private static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
-        // Check the scene state after it's loaded
-        CheckSceneState();
+        bool isInOrbit = CelestialTintStart.CheckSceneState();
+
+        if (isInOrbit) ActivateSun();
+        else DeactivateSun();
     }
 
-    private static IEnumerator DelayedCheckSceneState()
+    private static async void OnSceneUnloaded(Scene scene)
     {
-        // Wait for the current frame to end before checking the scene state
-        yield return new WaitForEndOfFrame();
-        CheckSceneState();
-    }
+        bool isInOrbit = await CelestialTintStart.DelayedCheckSceneStateAsync();
 
-    private static void OnSceneUnloaded(Scene scene)
-    {
-        // Delay the execution of CheckSceneState to ensure the scene has been fully unloaded
-        CoroutineHandlerSun.Instance.StartCoroutine(DelayedCheckSceneState());
+        if (isInOrbit) ActivateSun();
+        else DeactivateSun();
     }
 
     private static void ActivateSun()
     {
         if (sunCopy != null)
         {
-            sunCopy.SetActive(true); // Reactivate the existing copy
-            EnableLightComponent(sunCopy, true); // Ensure the Light component is active
+            sunCopy.SetActive(true);
+            EnableLightComponent(sunCopy, true);
         }
         else
         {
@@ -66,13 +45,13 @@ public static class VanillaMode
             if (originalProp != null)
             {
                 sunCopy = Object.Instantiate(originalProp, originalProp.transform.position, originalProp.transform.rotation);
-                sunCopy.name = sunName + "_copy"; // Set a distinct name for the copy
+                sunCopy.name = sunName + "_copy";
                 sunCopy.SetActive(true);
-                EnableLightComponent(sunCopy, true); // Ensure the Light component is active
+                EnableLightComponent(sunCopy, true);
             }
             else
             {
-                Debug.LogError("GameObject with name '" + sunName + "' not found.");
+                Debug.LogError("[CT VanillaMode] GameObject with name '" + sunName + "' not found.");
             }
         }
     }
@@ -81,8 +60,8 @@ public static class VanillaMode
     {
         if (sunCopy != null)
         {
-            EnableLightComponent(sunCopy, false); // Ensure the Light component is inactive
-            sunCopy.SetActive(false); // Deactivate the copy
+            EnableLightComponent(sunCopy, false);
+            sunCopy.SetActive(false);
         }
     }
 
@@ -95,25 +74,7 @@ public static class VanillaMode
         }
         else
         {
-            Debug.LogError("No Light component found on GameObject '" + obj.name + "'.");
-        }
-    }
-
-    // Coroutine handler to allow starting coroutines in static methods
-    public class CoroutineHandlerSun : MonoBehaviour
-    {
-        private static CoroutineHandlerSun _instance;
-        public static CoroutineHandlerSun Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    _instance = new GameObject("CoroutineHandlerSun").AddComponent<CoroutineHandlerSun>();
-                    DontDestroyOnLoad(_instance.gameObject);
-                }
-                return _instance;
-            }
+            Debug.LogError("[CT VanillaMode] No Light component found on GameObject '" + obj.name + "'.");
         }
     }
 }
